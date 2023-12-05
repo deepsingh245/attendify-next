af230bd838f2dc3541a651737d0c71838276509e
Deep Singh <deepsingh25244@gmail.com>
2023-12-06 01:01:39 +0530

    first

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+import Sidebar from './Sidebar'
+
+function Dashboard() {
+  return (
+    <div>
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default Dashboard
